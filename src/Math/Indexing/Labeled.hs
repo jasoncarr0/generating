3b86ba64d29c1910efda108a3d2a0dc627b8bb17
Jason Carr <jcarr250@gmail.com>
2016-02-28 21:33:11 -0500

    Adding instances, some duplicate code so might want to refactor

--- src/Math/Indexing/Labeled.hs
+++ src/Math/Indexing/Labeled.hs
@@ -12,13 +12,16 @@ import qualified Data.Map as Map
 
 newtype T l a = T (Map.Map l a)
 
+--Anything which outputs a map MUST run excludeZero
 instance (Ord l, ZT.C a, Add.C a) => Add.C (T l a) where
-    (T m1) + (T m2) = T $ Map.unionWith (+) m1 m2
+    (T m1) + (T m2) = T $ excludeZero $ Map.unionWith (+) m1 m2
     zero = T Map.empty
     negate (T m1) = T (fmap negate m1)
 instance (Show l, Show a) => Show (T l a) where
     show (T m1) = drop 1 $ Map.foldrWithKey showTerm "" m1 where
         showTerm l a str = ' ' : ((show l) ++ "^" ++ (show a) ++ str)
+instance (ZT.C a) => ZT.C (T l a) where
+    isZero (T m) = null $ excludeZero m
 instance (Eq l, Eq a) => Eq (T l a) where
     (T m1) == (T m2) = m1 == m2
 instance (Ord l, Add.C a, Ord a) => Ord (T l a) where
@@ -28,3 +31,6 @@ instance (Ord l, Add.C a, Ord a) => Ord (T l a) where
         
 norm :: Add.C a => T l a -> a
 norm (T m) = Map.foldr (+) zero m
+
+excludeZero :: ZT.C a => Map.Map l a -> Map.Map l a
+excludeZero = Map.filter (not . isZero)
--- src/Math/SparseSeries.hs
+++ src/Math/SparseSeries.hs
@@ -14,10 +14,14 @@ newtype T i a = T (Map.Map i a)
 
 instance Functor (T i) where
     fmap f (T g) = T (fmap f g)
-instance (Ord i, ZT.C i, Add.C i, Add.C a) => Add.C (T i a) where
-    (T m1) + (T m2) = T $ Map.unionWith (+) m1 m2
+instance (Ord i, ZT.C a, Add.C i, Add.C a) => Add.C (T i a) where
+    (T m1) + (T m2) = T $ excludeZero $ Map.unionWith (+) m1 m2
     zero = T Map.empty
     negate (T m1) = T (fmap negate m1)
 instance (Show l, Show a) => Show (T l a) where
     show (T m1) = drop 3 $ Map.foldrWithKey showTerm "" m1 where
         showTerm i a str = " + " ++ (show a) ++ (show i) ++ str
+        
+        
+excludeZero :: ZT.C a => Map.Map l a -> Map.Map l a
+excludeZero = Map.filter (not . isZero)
