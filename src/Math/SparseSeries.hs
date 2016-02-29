1ffc11fd61d9247f017d2840dc568bb0eed968b3
Jason Carr <jcarr250@gmail.com>
2016-02-28 20:36:08 -0500

    Replacing Indexing class with Ords and Sets, at least for sparse

--- src/Math/SparseSeries.hs
+++ src/Math/SparseSeries.hs
@@ -6,8 +6,9 @@ module Math.SparseSeries
 ) where
 import NumericPrelude
 import qualified Algebra.Ring as Ring
+import qualified Data.Map as Map
 
-data SpS i a = SpS (i -> a)
+newtype SpS i a = SpS (Map.Map i a)
 
 instance Functor (SpS i) where
 	fmap f (SpS g) = SpS (fmap f g)
