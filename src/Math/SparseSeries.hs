9547ba7bfe7f8309f97e3f490e7c956cbaafba24
Jason Carr <jcarr250@gmail.com>
2016-02-29 21:03:35 -0500

    Copying EQ and Ord

--- src/Math/SparseSeries.hs
+++ src/Math/SparseSeries.hs
@@ -18,9 +18,15 @@ instance (Ord i, ZT.C a, Add.C i, Add.C a) => Add.C (T i a) where
     (T m1) + (T m2) = T $ excludeZero $ Map.unionWith (+) m1 m2
     zero = T Map.empty
     negate (T m1) = T (fmap negate m1)
-instance (Show l, Show a) => Show (T l a) where
+instance (Show i, Show a) => Show (T i a) where
     show (T m1) = drop 3 $ Map.foldrWithKey showTerm "" m1 where
         showTerm i a str = " + " ++ (show a) ++ (show i) ++ str
+instance (Eq i, Eq a) => Eq (T i a) where
+    (T m1) == (T m2) = m1 == m2
+    
+-- TODO: Deal with equal bottoms, best is probably just to cut off
+instance (Ord i, Add.C a, Ord a) => Ord (T i a) where
+    compare (T m1) (T m2) = compare (Map.toAscList m1) (Map.toAscList m2)
         
         
 excludeZero :: ZT.C a => Map.Map l a -> Map.Map l a
